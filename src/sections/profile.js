a65bab3597bbe9088c7f7f51cb0fab6e7c6cf829
Jacqueline <leejjjacq@gmail.com>
2019-04-20 16:37:43 +1000

    Added profile template

--- src/sections/profile.js
+++ src/sections/profile.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class Profile extends Component {
     render() {
         return (
-            <p>Hello</p>
+            <div>
+                <img src="images/profile.jpg" class="margin-top-76 img-circle center-block profile_picture" alt="profile picture">
+                <h1 class="text-capitalize text-center">Jeanetter benson</h1>
+                <h3 class="text-capitalize text-center">UX/UI designer</h3>
+                <hr/>
+                <div class="text-center margin-bottom-135">
+                    <ul class="social-networks spin-icon">
+                        <li><a href="https://www.facebook.com/" class="icon-facebook">Facebook</a></li>
+                        <li><a href="https://twitter.com/" class="icon-twitter">Twitter</a></li>
+                        <li><a href="https://www.linkedin.com/" class="icon-linkedin">LinkedIn</a></li>
+                        <li><a href="https://pinterest.com" class="icon-pinterest">Pinterest</a></li>
+                        <li><a href="https://instagram.com" class="icon-instagram">Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
