bdec812dec010def2c301839b1980b2e05c73dda
Jacqueline <leejjjacq@gmail.com>
2019-04-20 16:31:54 +1000

    Updates

---
+++ precache-manifest.233ce0ba6914138c39f59e593278853b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1db08b9881b13f79b9a0",
+    "url": "/static/css/main.d13f3873.chunk.css"
+  },
+  {
+    "revision": "1db08b9881b13f79b9a0",
+    "url": "/static/js/main.a18425c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "9290caa23638d5ff31dd70717d7ece05",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c91b0a3d97695b210ef1b0cc9a44196.js"
+  "/precache-manifest.233ce0ba6914138c39f59e593278853b.js"
 );
 
 workbox.clientsClaim();
